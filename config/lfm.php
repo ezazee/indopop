c6ca92c60e11a0bd027e4d8087aa2729b9d53aa9
Risqiyyyy <raisafabriani05@gmail.com>
2025-02-17 17:19:15 +0700

    chek

--- config/lfm.php
+++ config/lfm.php
@@ -26,7 +26,7 @@ return [
     |
      */
 
-    'allow_private_folder'     => true,
+    'allow_private_folder'     => false,
 
     // Flexible way to customize client folders accessibility
     // If you want to customize client folders, publish tag="lfm_handler"
@@ -49,31 +49,27 @@ return [
         'file'  => [
             'folder_name'  => 'files',
             'startup_view' => 'list',
-            'max_size'     => 50000, // size in KB
+            'max_size'     => 1500, // size in KB
             'thumb' => true,
             'thumb_width' => 80,
             'thumb_height' => 80,
             'valid_mime'   => [
                 'image/jpeg',
-                'image/pjpeg',
+                'image/jpg',
                 'image/png',
-                'image/gif',
-                'application/pdf',
-                'text/plain',
             ],
         ],
         'image' => [
             'folder_name'  => 'photos',
             'startup_view' => 'grid',
-            'max_size'     => 50000, // size in KB
+            'max_size'     => 1500, // size in KB
             'thumb' => true,
-            'thumb_width' => 80,
-            'thumb_height' => 80,
+            'thumb_width' => 123,
+            'thumb_height' => 123,
             'valid_mime'   => [
                 'image/jpeg',
                 'image/pjpeg',
                 'image/png',
-                'image/gif',
             ],
         ],
     ],
@@ -96,22 +92,22 @@ return [
 
     'disk'                     => 'public',
 
-    'rename_file'              => false,
+    'rename_file'              => true,
 
-    'rename_duplicates'        => false,
+    'rename_duplicates'        => true,
 
-    'alphanumeric_filename'    => false,
+    'alphanumeric_filename'    => true,
 
-    'alphanumeric_directory'   => false,
+    'alphanumeric_directory'   => true,
 
-    'should_validate_size'     => false,
+    'should_validate_size'     => true,
 
     'should_validate_mime'     => true,
 
     // behavior on files with identical name
     // setting it to true cause old file replace with new one
     // setting it to false show `error-file-exist` error and stop upload
-    'over_write_on_duplicate'  => false,
+    'over_write_on_duplicate'  => true,
 
     // mimetypes of executables to prevent from uploading
     'disallowed_mimetypes' => ['text/x-php', 'text/html', 'text/plain'],
@@ -136,13 +132,13 @@ return [
     // Create thumbnails automatically only for listed types.
     'raster_mimetypes'         => [
         'image/jpeg',
-        'image/pjpeg',
+        'image/jpg',
         'image/png',
     ],
 
-    'thumb_img_width'          => 200, // px
+    'thumb_img_width'          => 123, // px
 
-    'thumb_img_height'         => 200, // px
+    'thumb_img_height'         => 123, // px
 
     /*
     |--------------------------------------------------------------------------
